3da3e3bcf119bbaddd6638305e2d4fe21f0b74d3
manie204 <mani.e@protonmail.com>
2021-11-26 10:47:49 +0100

    Change include statement of header file

--- src/Options.cpp
+++ src/Options.cpp
@@ -1,4 +1,4 @@
-#include <Options.hpp>
+#include "Options.hpp"
 
 #include <iostream>
 #include <sstream>
--- src/mpidb.cpp
+++ src/mpidb.cpp
@@ -3,7 +3,7 @@
 
 #include <unistd.h>
 
-#include <Options.hpp>
+#include "Options.hpp"
 
 int main(int argc, char ** argv)
 {
